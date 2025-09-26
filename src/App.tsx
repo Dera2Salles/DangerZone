0031131ccf555b9ac73f0e490d97a02e52d0a935
Dera2S <dera.ah.14@gmail.com>
2025-09-26 10:37:59 +0300

    feat : quartier component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { Quartier } from "./CartRow";
+
 const App = () => {
-  return <div className=" bg-zinc-100 h-full overflow-x-hidden"></div>;
+  return <div className=" bg-zinc-100 h-full overflow-x-hidden">
+    <Quartier/>
+  </div>;
 };
 export default App;
---
+++ src/CartRow.tsx
@@ -0,0 +1,21 @@
+import { AlertTriangleIcon, ShieldCheckIcon, Skull } from 'lucide-react';
+
+interface Quartier {
+  name: string;
+  dangerLevel: number;
+  warninglevel: number;
+  safeNumber: number;
+}
+
+export const Quartier = () => {
+  return (
+    <div className="w-full md:w-1/2 bg-card  text-card-foreground items-center justify-between  flex flex-col gap-10 rounded-4xl border-4 py-5 px-5 shadow-2xs">
+      <p>Sabotsy Namehana</p>
+      <div className=" flex">
+        <Skull className=" text-red-600" />
+        <AlertTriangleIcon className="text-yellow-500" />
+        <ShieldCheckIcon className="text-green-500" />
+      </div>
+    </div>
+  );
+};
