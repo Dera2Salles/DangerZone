02d25536e3f8dc705b6a75d1a0acfdb1bbc920ee
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-26 12:40:26 +0300

    add counter

--- src/CartRow.tsx
+++ src/CartRow.tsx
@@ -1,21 +1,41 @@
 import { AlertTriangleIcon, ShieldCheckIcon, Skull } from 'lucide-react';
+import { useState, type ReactNode } from 'react';
 
 interface Quartier {
   name: string;
-  dangerLevel: number;
-  warninglevel: number;
-  safeNumber: number;
+};
+type Level ={
+  id: string,
+  icon: ReactNode;
+  count: number,
 }
 
+
 export const Quartier = () => {
+  const quartier: Quartier[] =  [
+    {name: "Sabotsy Namehana"} ,
+    {name: "Sabotsy Namehana" },
+    {name: "Sabotsy Namehana" },
+    {name: "Sabotsy Namehana" },
+  ]
+  
+  const level : Level[]=[
+    { id: "dangerLevel", icon:<AlertTriangleIcon/>, count: 0},
+    { id: "warninglevel", icon:<ShieldCheckIcon/>, count: 0},
+    { id: "safeNumber", icon:<Skull/>, count: 0}
+  ]
+  const [count, setCount]= useState<number>(0);
+  const handleClick = () => {
+    setCount(count+1);
+  };
   return (
-    <div className="w-full md:w-1/2 bg-card  text-card-foreground items-center justify-between  flex flex-col gap-10 rounded-4xl border-4 py-5 px-5 shadow-2xs">
-      <p className='font-semibold text-3xl'>Sabotsy Namehana</p>
-      <div className=" flex">
-        <Skull className=" text-red-600" />
-        <AlertTriangleIcon className="text-yellow-500" />
-        <ShieldCheckIcon className="text-green-500" />
-      </div>
+    <div className='flex flex-col gap-5 h-screen w-screen justify-center items-center'>
+        {quartier.map((quartier ,key ) =>(<div className='flex gap-5 flex-col border-4 border-gray-600 rounded-2xl m-2 p-4' key={key}>
+          <h2 className='font-semibold text-3xl '>{quartier.name}</h2>
+          <div className="flex gap-2 " onClick={handleClick}>
+          {level.map((level ,key ) =>(<div id={level.id} className='flex cursor-pointer ' key={key}>{level.icon}: <p className='text-black'>{count}</p></div>))}
+          </div>
+        </div>))}
     </div>
   );
 };
