faee9dcdb5a327b72719d3cd291453eabfa354ee
Tatsumi96 <tatsumiyamato002@gmail.com>
2025-09-26 13:34:38 +0300

    fix: fixed counter

--- src/CartRow.tsx
+++ src/CartRow.tsx
@@ -19,21 +19,20 @@ export const Quartier = () => {
     {name: "Sabotsy Namehana" },
   ]
   
-  const level : Level[]=[
+  const [levels,setLevels] = useState<Level[]>([
     { id: "dangerLevel", icon:<AlertTriangleIcon/>, count: 0},
     { id: "warninglevel", icon:<ShieldCheckIcon/>, count: 0},
     { id: "safeNumber", icon:<Skull/>, count: 0}
-  ]
-  const [count, setCount]= useState<number>(0);
-  const handleClick = () => {
-    setCount(count+1);
+  ])
+  const handleClick = (id:string) => {
+    setLevels(prev => prev.map(level => level.id === id ? {...level, count: level.count + 1} : level));
   };
   return (
     <div className='flex flex-col gap-5 h-screen w-screen justify-center items-center'>
         {quartier.map((quartier ,key ) =>(<div className='flex gap-5 flex-col border-4 border-gray-600 rounded-2xl m-2 p-4' key={key}>
           <h2 className='font-semibold text-3xl '>{quartier.name}</h2>
-          <div className="flex gap-2 " onClick={handleClick}>
-          {level.map((level ,key ) =>(<div id={level.id} className='flex cursor-pointer ' key={key}>{level.icon}: <p className='text-black'>{count}</p></div>))}
+          <div className="flex gap-2 ">
+          {levels.map((level ,key ) =>(<div onClick={()=>handleClick(level.id)} id={level.id} className='flex cursor-pointer ' key={key}>{level.icon}: <p className='text-black'>{level.count}</p></div>))}
           </div>
         </div>))}
     </div>
